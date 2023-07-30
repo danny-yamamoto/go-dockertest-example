a0cf79cd3f878f3b7093f7525dad666c2efca6cc
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-07-30 10:56:05 +0000

    newly generated queries.

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
+	"reflect"
 
+	"github.com/danny-yamamoto/go-dockertest-example/tutorial"
 	_ "github.com/lib/pq"
 )
 
@@ -18,11 +21,27 @@ const (
 
 func main() {
 	fmt.Println("hello")
-	//ctx := context.Background()
+	ctx := context.Background()
 	connStr := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable", HOST, PORT, DBNAME, USER, PASSWORD)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
+	queries := tutorial.New(db)
+	authors, err := queries.ListAuthors(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(authors)
+	insert, err := queries.CreateAuthor(ctx, tutorial.CreateAuthorParams{Name: "danny", Bio: sql.NullString{String: "SRE", Valid: true}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(insert)
+	fetched, err := queries.GetAuthor(ctx, insert.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(reflect.DeepEqual(insert, fetched))
 }
