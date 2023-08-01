da9b927ef95d4a50540c427c167936f30dbea203
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-08-01 23:59:35 +0000

    Move pool.

--- cmd/server/server_test.go
+++ cmd/server/server_test.go
@@ -10,11 +10,12 @@ import (
 
 	_ "github.com/lib/pq"
 	"github.com/ory/dockertest/v3"
+	dc "github.com/ory/dockertest/v3/docker"
 )
 
 const (
 	DINDHOST     = "localhost"
-	DINDPORT     = "5432"
+	DINDPORT     = "0"
 	DINDDBNAME   = "postgres"
 	DINDUSER     = "postgres"
 	DINDPASSWORD = "postgres"
@@ -27,15 +28,33 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		log.Fatalf("Unable to connect to docker: %s\n", err)
 	}
+
 	err = pool.Client.Ping()
 	if err != nil {
 		log.Fatalf("docker does not accept communication: %s\n", err)
 	}
+
+	ro := &dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "14.8",
+		Env:        []string{"POSTGRES_USER=postgres", "POSTGRES_PASSWORD=postgres", "listen_addresses = '*'"},
+	}
+
+	// Delete dind's postgres so that there is no postgres left.
+	resource, err := pool.RunWithOptions(ro,
+		func(hc *dc.HostConfig) {
+			hc.AutoRemove = true
+			hc.RestartPolicy = dc.RestartPolicy{Name: "no"}
+		})
 	if err != nil {
 		log.Fatalf("Failed to start docker: %s\n", err)
 	}
-	databaseUrl := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable", DINDHOST, DINDPORT, DINDDBNAME, DINDUSER, DINDPASSWORD)
-	fmt.Printf("databaseUrl: %s\n", databaseUrl)
+
+	// The port to be opened is automatically waved.
+	hostAndPort := resource.GetHostPort("5432/tcp")
+	log.Printf("hostAndPort: %s\n", hostAndPort)
+	databaseUrl := fmt.Sprintf("postgres://postgres:postgres@%s/postgres?sslmode=disable", hostAndPort)
+	log.Printf("databaseUrl: %s\n", databaseUrl)
 	if err := pool.Retry(func() error {
 		time.Sleep(time.Second * 10)
 		db, err := sql.Open("postgres", databaseUrl)
@@ -47,5 +66,8 @@ func TestMain(m *testing.M) {
 		log.Fatalf("Unable to connect to database: %s\n", err)
 	}
 	code := m.Run()
+	if err := pool.Purge(resource); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
 	os.Exit(code)
 }
